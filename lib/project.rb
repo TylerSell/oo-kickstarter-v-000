b9c6413fe50871833bc8bbca0568cf260ebf3bf0
tylerselldesign@gmail.com <tylerselldesign@gmail.com>
2018-09-13 04:26:50 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -2,7 +2,11 @@ class Project
   attr_accessor :backers
   
   def initialize(title)
-    @backers
+    @backers = []
     @title = title 
+  end
+  
+  def self.add_backer(backer)
+    @backers << backer 
   end
 end
